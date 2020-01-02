46a5028beed5784fc19e2d27daf7b65afde8863a
Jason Oakley <waulok@gmail.com>
2020-01-03 07:56:15 +1100

    Updated score code

--- score.asm
+++ score.asm
@@ -2,7 +2,7 @@ XDEF _drawscore
 _drawscore:
 
          LD    HL,2
-         ADD   HL,SP               ; skip over return address on stack
+         ADD   HL,SP                ; skip over return address on stack
          LD    A,(HL)               ; A = spriteno
          LD    C,A                  ; C = spriteno  **
          INC   HL
@@ -10,63 +10,34 @@ _drawscore:
          LD    E,(HL)
          INC   HL
          LD    D,(HL)               ; DE = screen location
-         PUSH  DE
-         POP   HL   
-			LD		A,5
-			LD		(counter),A
-         LD    A,C
-			CP    $00
-			LD		DE,numbers
-			JR		Z,showit
-countit: INC   DE
-			INC   DE
-			INC   DE
-			INC   DE
-			INC   DE
-			DEC   A   
-			JR	   NZ,countit
-showit:
-			LD	   A,(DE)
-			INC	DE
-			LD 	(HL),A
-			LD	   BC,32			 ; A line's length
-			ADD	HL,BC
-			LD	   A,(counter)
-			DEC	A
-			LD	   (counter),A
-			JR	   NZ,showit
-			RET
-
-counter: defb 0x00
+         LD    A,C                  ; BC = 5*C
+         ADD   A,A
+         ADD   A,A
+         ADD   A,C
+         LD    C,A
+         LD    B,0
+         LD    HL,numbers
+         ADD   HL,BC
+         LD    C,5
+ 
+ .SHOWIT
+         LDI
+         RET PO
+         LD A,31
+         ADD A,E
+         LD E,A
+         JP NC, SHOWIT
+         INC D
+         JP SHOWIT
 
 numbers:
-
-   ; 0
-   defb $3f, $33, $33, $33, $3f
-
-   ; 1
-      defb $3c, $0c, $0c, $0c, $3f
-
-   ; 2
-      defb $3f, $03, $0c, $30, $3f
-
-   ; 3
-      defb $3f, $03, $0f, $03, $3f
- 
-   ; 4
-      defb $33, $33, $3f, $03, $03
-
-   ; 5
-      defb $3f, $30, $0c, $03, $3f
-
-   ; 6
-      defb $3f, $30, $3f, $33, $3f
-
-   ; 7
-      defb $3f, $03, $03, $03, $03
-
-   ; 8
-      defb $3f, $33, $0c, $33, $3f
-
-   ; 9
-      defb $3f, $33, $3f, $03, $3f
+defb $15, $11, $11, $11, $15       ; 0
+defb $14, $04, $04, $04, $15       ; 1
+defb $15, $01, $04, $10, $15       ; 2
+defb $15, $01, $05, $01, $15       ; 3
+defb $11, $11, $15, $01, $01       ; 4
+defb $15, $10, $15, $01, $15       ; 5
+defb $15, $10, $15, $11, $15       ; 6
+defb $15, $01, $01, $01, $01       ; 7
+defb $15, $11, $04, $11, $15       ; 8
+defb $15, $11, $15, $01, $15       ; 9
